17cf34efb42593ed7587b6990f1eb46bbff43230
Kohei Morita <moritakouhei@graffer.jp>
2024-12-29 22:17:36 +0900

    fix: embed path

--- lib/libinjection_linux_amd64.go
+++ lib/libinjection_linux_amd64.go
@@ -5,5 +5,5 @@ package lib
 
 import _ "embed"
 
-//go:embed vendor/libinjection/linux_aarch64/libinjection.so
+//go:embed vendor/libinjection/linux_amd64/libinjection.so
 var LibinjectionSharedLib []byte
